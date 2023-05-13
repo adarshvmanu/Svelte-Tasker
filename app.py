758fae798a8d983ffe105ff96ebc4e9d2df0d9fa
adarshvmanu <adarshvmanu@gmail.com>
2023-05-13 22:44:53 +0530

    Day 2

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(25))
+    description = db.Column(db.String(100))
+
+    def repr(self):
+        return f'<task: {self.name}>'
 
 @app.route('/')
 
