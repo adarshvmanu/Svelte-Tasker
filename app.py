3bfae97f010117e7d8ed921e8bddf3f62c445947
adarshvmanu <adarshvmanu@gmail.com>
2023-05-15 11:01:57 +0530

    Day 3

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
@@ -11,14 +12,23 @@ class task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(25))
     description = db.Column(db.String(100))
+    date = db.Column(db.String(10))
+    time = db.Column(db.String(5))
 
     def repr(self):
         return f'<task: {self.name}>'
 
 @app.route('/')
-
 def index():
-    return render_template('base.html')
+    return render_template('index.html', name = "Jikuttan")
+
+@app.route('/tasks')
+def tasks():
+    return 'Here is the list of tasks'
+
+@app.route('/tasks/<string:name>')
+def task(name):
+    return render_template('tasks.html', name = name)
 
 if __name__ == '__main__':
     app.run(debug=True)
